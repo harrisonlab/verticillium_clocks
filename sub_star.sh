a8df28f6dad8ebbe8fc7e22864086ca18d71559c
EmmaCL <emma.cascant-lopez@emr.ac.uk>
2017-02-08 11:32:09 +0000

    completed star

--- sub_star.sh
+++ sub_star.sh
@@ -3,66 +3,73 @@
 
 #$ -S /bin/bash
 #$ -cwd
-#$ -pe smp 16
-#$ -l virtuak_free=1.2G
-#$ -l
+#$ -pe smp 8
+#$ -l virtual_free=1.2G
 
 # ---------------
 # Step 1
 # Collect inputs
 # ---------------
-
-InGenome=$(basname $1)
-InGff=$(basname $2)
-InReadF=$(basname $3)
-InReadR=$(basname $4)
-GenomeDir=$(basname $5)
-OutDir=$6
+echo "Hello"
+InGenome=$(basename $1)
+InGff=$(basename $2)
+InReadF=$(basename $3)
+InReadR=$(basename $4)
+# genomeDir=$(basname $5)
+OutDir=$5
 
 CurDir=$PWD
 WorkDir=$TMPDIR/star
-mkdir -p $WorkDir
+genomeDir=$WorkDir/index
+echo "$WorkDir"
+echo "$genomeDir"
+mkdir -p $genomeDir
+ls $genomeDir
 cd $WorkDir
 
 cp $CurDir/$1 $InGenome
 cp $CurDir/$2 $InGff
 cp $CurDir/$3 $InReadF
 cp $CurDir/$4 $InReadR
-cp $CurDir/$5 $GenomeDir
-cp $CurDir/$6 $OutDir
+# cp $CurDir/$5 $GenomeDir
+cp $CurDir/$5 $OutDir
+
+
 
 # ---------------
 # Step 2
 # Create the Index File
 # ---------------
-
+echo "Building index file"
 ParentFeature="Parent"
 # ParentFeature="ID"
 
-./STAR \
+/home/groups/harrisonlab/project_files/verticillium_dahliae/clocks/STAR \
 --runMode genomeGenerate \
---genomeDir $GenomeDir \
+--genomeDir $genomeDir \
 --genomeFastaFiles $InGenome \
 --sjdbGTFtagExonParentTranscript $ParentFeature \
---sjdbGTFfile $InGff
---runThreadN 16 \
---sjdbOverhang ReadLength-1
+--sjdbGTFfile $InGff \
+--runThreadN 8 \
+--sjdbOverhang 99
 
 # ---------------
 # Step 2=3
 # Run STAR
 # ---------------
 
-./STAR \
---genomeDir $GenomeDir \
---outFileNamePrefix $OutDir \
+echo "Aligning RNAseq reads"
+
+/home/groups/harrisonlab/project_files/verticillium_dahliae/clocks/STAR \
+--genomeDir $genomeDir \
+--outFileNamePrefix star_aligment \
 --readFilesCommand zcat \
 --readFilesIn $InReadF $InReadR \
 --outSAMtype SAM \
---runThreadN 16
+--runThreadN 8
 
 
-rm -r $GenomeDir
+rm -r $genomeDir
 rm $InGenome
 rm $InGff
 rm $InReadF
