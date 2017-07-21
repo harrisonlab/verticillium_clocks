775c14ad691fd6cabe5753f23c8d9124c02cf097
Thomas Adams <adamst@emr.ac.uk>
2017-07-21 11:38:34 +0100

    Adds additional comment for testing presence & LFC

--- parse_RNASeq_3-way.py
+++ parse_RNASeq_3-way.py
@@ -76,6 +76,7 @@ genes = set(genes_list)
 a = numpy.array(["Gene_Name", "24hr", "48hr", "96hr"])
 
 #These commands test if a gene is present in the DEG file or not, then if it has an absolute log2 fold change greater than the threshold of 1
+#It then adds the name of the gene and the result of the test (1 or 0) to a numpy array
 
 for x in genes:
     to_add = []
